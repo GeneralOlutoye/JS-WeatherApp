d0701ca2d156ab4f6ee478ddd0df9af7131ca5cc
Joel Olutoye <donronald27@gmail.com>
2022-08-15 12:47:50 +0100

    Update

--- index.js
+++ index.js
@@ -6,6 +6,51 @@ const date = document.querySelector('.date')
 
 let city
 getBtn.addEventListener('click', () => {
+
+    city = TxtArea.value
+
+    if(city != ''){
+        city = TxtArea.value
+        fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5d18828185d71c238c11c35c2d12b7eb&units=metric`)
+        .then (weather =>{
+        return weather.json()
+    })
+    .then(res =>{
+        display.textContent=`${Math.round(res.main.temp)}°C`
+        locationBox.textContent=`${res.name}, ${res.sys.country}`;
+    })
+
+    TxtArea.value=''
+    
+    const dateBuilder = (d) => {
+        let months = ["January" , "Februaury" , "March" , "April" , "May" , "June" , "July" , "August" , "September" ,
+                        "October" , "November" , "December"];
+        let days = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"];
+    
+    
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+    
+        return `${day} ${date}, ${month} ${year}`
+    }
+
+    const jsDate = `${dateBuilder(new Date())}`
+
+    date.innerHTML= jsDate
+
+    document.querySelector('.container').style.backgroundImage= "url('./images/warm-bg.jpg')"
+    }else{
+        TxtArea.value= ''
+        display.textContent= 'Enter Valid City'
+        document.querySelector('.container').style.backgroundImage= "url('./images/cold-bg.jpg')"
+    }
+
+})
+
+TxtArea.addEventListener("onkeypress", (event)=>{
+    event.preventDefault()
     city = TxtArea.value
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5d18828185d71c238c11c35c2d12b7eb&units=metric`)
     .then (weather =>{
@@ -20,6 +65,6 @@ getBtn.addEventListener('click', () => {
     TxtArea.value=''
 
     document.querySelector('.container').style.backgroundImage= "url('./images/warm-bg.jpg')"
-
-
 })
+
+
